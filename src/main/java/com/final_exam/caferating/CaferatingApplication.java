970f1c4bcc96d1e8867dcd78f598ccaaad23ac87
Baktygul <bakulyakozhoshova@gmail.com>
2020-08-15 12:12:15 +0600

    создание проекта

---
+++ src/main/java/com/final_exam/caferating/CaferatingApplication.java
@@ -0,0 +1,13 @@
+package com.final_exam.caferating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaferatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaferatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/final_exam/caferating/CaferatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_exam.caferating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaferatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
