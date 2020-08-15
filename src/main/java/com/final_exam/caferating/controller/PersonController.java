6b992c1cd413040450a5922d41cc45950a80c0c1
Baktygul <bakulyakozhoshova@gmail.com>
2020-08-15 15:56:16 +0600

    добавила PersonController и SecurityConfig

---
+++ src/main/java/com/final_exam/caferating/controller/PersonController.java
@@ -0,0 +1,56 @@
+package com.final_exam.caferating.controller;
+
+import com.final_exam.caferating.form.PersonRegisterForm;
+import com.final_exam.caferating.repo.PersonRepo;
+import com.final_exam.caferating.service.PersonService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@AllArgsConstructor
+@Controller
+public class PersonController {
+
+    private final PersonService personService;
+
+    @GetMapping
+    public String mainPage(Model model, Principal principal){
+
+        personService.checkUserPresence(model, principal);
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model, Principal principal){
+
+        personService.checkUserPresence(model, principal);
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model, Principal principal){
+
+        personService.checkUserPresence(model, principal);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid PersonRegisterForm personRegisterForm,
+                          BindingResult validationResult,
+                          RedirectAttributes attributes){
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/register";
+        }
+        personService.registerPerson(personRegisterForm);
+        return "redirect:/login";
+    }
+
+}
