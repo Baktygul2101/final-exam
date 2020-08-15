b28dd18e2df03dbd36d5a44b71c81afe5aaffd0b
Baktygul <bakulyakozhoshova@gmail.com>
2020-08-15 19:59:32 +0600

    исправление

--- src/main/java/com/final_exam/caferating/repo/ReviewRepo.java
+++ src/main/java/com/final_exam/caferating/repo/ReviewRepo.java
@@ -7,11 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ReviewRepo extends JpaRepository<Review, Long> {
-
-    Optional<Review> findByName(String name);
 
     Page<Review> getAllByPersonEmail(Pageable pageable, String email);
 }
